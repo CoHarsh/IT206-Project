5fd51e604354d24cd1fe71e293fd72a69f109e30
Harsh <harshmp492@gmail.com>
2021-05-25 08:36:31 +0530

    update with linkedlist

--- Text-analizer.cpp
+++ Text-analizer.cpp
@@ -1,22 +1,22 @@
 #include<bits/stdc++.h>
-
 using namespace std;
 
     
-    class list_for_characters;
+class list_for_characters;
 
     class node_1
     {
          private:
          char c;
-         int no;
+         int no = 0;
          node_1 * next;
 
-         node_1(char ch){
-             c=ch;
-             no=0;
-             next=NULL;
-         }
+//Constructor
+        //  node_1(char ch){
+        //      c=ch;
+        //      no=0;
+        //      next=NULL;
+        //  }
          friend list_for_characters;
     };
 
@@ -24,35 +24,94 @@ using namespace std;
     {
         public:
         node_1 * head ;
-        void crete();
-        void dis();
+        node_1 *travesal;
+        int arr[200] = {0};
+        int space = 0;
+        void crete(string s);
+        void display();
          
     };
     
-    void list_for_characters::crete()
+    void list_for_characters::crete(string s)
     {
-        head = new node_1('A');
+        int n = s.length();
+        int i=0;
+        while(n--)
+        {
+        // if(arr[(int)s[i] - 48] == 0)
+        // {
+            // arr[i] = 1;
+            if(s[i] == ' ')
+            {
+                space++;
+                i++;
+                continue;
+                
+            }
+            if(i==0)
+            {
+                node_1*temp = new node_1;
+                temp->c = s[i];
+                temp->no += 1;
+                temp->next = NULL; 
+                head = temp;
+                travesal = temp;
+                arr[(int)s[i] - 48] = 1;
 
-        node_1 * B = new node_1('B');
-        head->next=B;
+            }
+            else{
+                if(arr[(int)s[i] - 48] == 0)
+                {
+                node_1*temp = new node_1;
+                temp->c = s[i];
+                temp->no += 1;
+                temp->next = NULL; 
+                travesal->next = temp;
+                travesal = temp;
+                arr[(int)s[i] - 48] = 1;
+                }
+                else
+                {
+                    node_1*temp = new node_1;
+                    temp = head;
+                    while(temp->c != s[i])
+                    {
+                        temp = temp->next;
+                        
+                    }
+                    temp->no += 1;
+                    // arr[(int)s[i] - 48] = 1;
+                    
+
+                }
+
+            }
+            i++;
+            
+        // } 
+
+        }
 
     }
 
-    void list_for_characters::dis(){
+    void list_for_characters::display(){
         node_1 * temp = head;
         while(temp!=NULL){
-            cout<<temp->c<<endl;
+            cout<<temp->c<<" = "<<temp->no<<endl;
             temp = temp->next;
         }
-        head->no++;
-        cout<<head->no;
+        // head->no++;
+        // cout<<head->no;
+        cout<<"the spaces are "<<space<<endl;
         
     }
     
 
 int main(){
-            list_for_characters a;
-            a.crete();
-            a.dis();
+    string s;
+    getline(cin,s);
+    list_for_characters s1;
+    s1.crete(s);
+    s1.display();
     return 0;
 }
